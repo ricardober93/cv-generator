eaa73e33b9113e847c0ff2bf74718d3bde893725
Ricardo Bermudez Tellez <39892056+ricardober93@users.noreply.github.com>
2025-03-25 21:43:32 -0500

    fix(logout): add href and cursor-pointer to logout link
    The logout link was missing the `href` attribute, which prevented it from functioning as a link. Additionally, the `cursor-pointer` class was added to improve the user experience by indicating that the element is clickable.

--- frontend/src/components/logout/Logout.tsx
+++ frontend/src/components/logout/Logout.tsx
@@ -1,7 +1,9 @@
 import { RiLogoutBoxRLine } from "react-icons/ri"
 
 export function Logout() {
-  return <a className="font-bold rounded-lg px-4 py-2 bg-zinc-800 text-white justify-center">
+  return <a
+    href="/api/logout"
+    className="font-bold rounded-lg px-4 py-2 bg-zinc-800 text-white justify-center cursor-pointer">
     <RiLogoutBoxRLine className="inline-block mr-2" />
     Logout
   </a>;
